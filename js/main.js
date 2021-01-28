78c67fb34ee365191426520b6c9eaef56feb2e86
CCrisBIB <cristinabiglesias@gmail.com>
2021-01-28 11:06:38 +0100

    Add 'click', handleButtonElement event and update feedback function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-const inputElement = document.querySelector('');
+/* Traigo los elementos del html que utilzaré y los convierto en constantes globales (cqs): js-input + js-button + js-clues + js-counter. */
+
+const inputElement = document.querySelector('.js-input');
+const buttonElement = document.querySelector('.js-button');
+const cluesElement = document.querySelector('.js-clues');
+const counterElement = document.querySelector('.js-counter');
+
+const getRandomNumber = (max) => {
+    return Math.ceil(Math.random() * max);
+}
+
+const randomNumber = getRandomNumber(100)
+console.log(randomNumber);
+
+function handleButtonElement(ev) {
+    ev.preventDefault()
+    updateFeedback();
+    updateCounter();
+}
+
+function updateFeedback() {
+    const inputValue = parseInt(inputElement.value);
+    console.log(inputValue);
+    if (inputValue === randomNumber) {
+        cluesElement.innerHTML = "Has ganado campeona!!!";
+    }
+    else if (inputValue < randomNumber) {
+        cluesElement.innerHTML = "Demasiado bajo."
+    }
+    else if (inputValue > randomNumber) {
+        cluesElement.innerHTML = "Demasiado alto."
+    }
+    else if (inputValue > 100){
+        cluesElement.innerHTML = "El número debe estar entre 1 y 100."
+    }
+    else if (inputValue < 100){
+        cluesElement.innerHTML = "El número debe estar entre 1 y 100."
+    }
+    else if (inputValue) {
+        cluesElement.innerHTML = "El número debe estar entre 1 y 100."
+    }
+    else {
+        cluesElement.innerHTML = "El número debe estar entre 1 y 100."
+    }
+}
+
+function updateCounter() {
+    
+}
+
+buttonElement.addEventListener('click', handleButtonElement);
+
+
